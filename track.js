f332cc4df0c94986f19092e9ea89b9ea7e0f40f3
Alan Darma <40722633+alandrma@users.noreply.github.com>
2026-07-09 09:59:20 +0700

    Update track.js

--- track.js
+++ track.js
@@ -1,19 +1,5 @@
 function gv() {
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf("affclick=") == 0) {
-            return c.substring("affclick=".length, c.length);
-        }
-    }
-    if (localStorage.getItem('affclick')) {
-        return localStorage.getItem('affclick');
-    }
-    return "";
+    
 }
 
 (function(){
